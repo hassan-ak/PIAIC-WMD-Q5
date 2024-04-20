276e9b7cff2b204945fc1f071a1c7b03b0a2dfa4
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2024-04-20 18:39:53 +0500

    class02-20240317

---
+++ class02-20240317/10_microservice_helloworld/fastapi-helloworld/fastapi_helloworld/main.py
@@ -0,0 +1,23 @@
+# fastapi_neon/main.py
+
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Hello World API",
+    version="0.0.1",
+    servers=[
+        {
+            "url": "http://0.0.0.0:8000",  # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Development Server",
+        },
+        {
+            "url": "https://sterling-maximum-falcon.ngrok-free.app",  # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Deployment Server",
+        },
+    ],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
